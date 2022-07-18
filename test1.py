b6daf21a4008394744db7766059e61c733a5d9b2
miqbalatif <miqbalatif@gmail.com>
2022-07-18 06:32:00 +0500

    this is my first code change

--- test1.py
+++ test1.py
@@ -4,6 +4,10 @@ db = client.test
 print(db)
 
 d={'name':'ali', 'age':20, 'job':'student', 'marital':'single', 'education':'highschool'}
+d1={'name':'ali', 'age':20, 'job':'student', 'marital':'single', 'education':'highschool'}
+d2={'name':'ali', 'age':20, 'job':'student', 'marital':'single', 'education':'highschool'}
+d3={'name':'ali', 'age':20, 'job':'student', 'marital':'single', 'education':'highschool'}
+d4={'name':'ali', 'age':20, 'job':'student', 'marital':'single', 'education':'highschool'}
 db1 = client['mongotest']
 coll = db1['test']
 coll.insert_one(d)
