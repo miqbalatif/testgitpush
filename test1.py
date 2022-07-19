261d31833b518c68c56f55df75654f5be9f6d1a2
miqbalatif <miqbalatif@gmail.com>
2022-07-19 06:42:31 +0500

    done

--- test1.py
+++ test1.py
@@ -8,6 +8,7 @@ d1={'name':'ali', 'age':20, 'job':'student', 'marital':'single', 'education':'hi
 d2={'name':'ali', 'age':20, 'job':'student', 'marital':'single', 'education':'highschool'}
 d3={'name':'ali', 'age':20, 'job':'student', 'marital':'single', 'education':'highschool'}
 d4={'name':'ali', 'age':20, 'job':'student', 'marital':'single', 'education':'highschool'}
+d5={'name':'ali', 'age':20, 'job':'student', 'marital':'single', 'education':'highschool'}
 db1 = client['mongotest']
 coll = db1['test']
 coll.insert_one(d)
